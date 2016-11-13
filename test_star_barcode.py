e618f6c4e3eefa62ebc689e49b7ddaf2def4fc0d
Rob Wells <rob@robjwells.com>
2016-11-13 17:02:19 +0000

    add failing tests for barcode filename

--- test_star_barcode.py
+++ test_star_barcode.py
@@ -32,5 +32,55 @@ class TestHeader(unittest.TestCase):
             header,
             'MSTAR 2016-11-12 SAT 1.2')
 
+
+class TestFilename(unittest.TestCase):
+    """Test barcode_filename function for naming barcode
+
+    barcode_filename should take a datetime object and an int,
+    representing the sequence number of the edition, and
+    produce a string as so:
+        Barcode_%G-W%V-%u_SS.pdf
+    Where %G is the ISO year number, %V the ISO week number,
+    and %u the ISO weekday number. SS is the sequence int.
+    """
+
+    def test_ordinary(self):
+        """Boring test of a standard date and sequence"""
+        date = datetime(2016, 11, 12)
+        seq = 36
+        name = star_barcode.barcode_filename(date, seq)
+        self.assertEqual(
+            name,
+            'Barcode_2016-W11-12_36.pdf'
+            )
+
+    def test_wrong_sequence(self):
+        """barcode_filename should throw when sequence is wrong
+
+        A ValueError exception should be raised if the second digit
+        of the sequence (sequence % 10) is not the same as the ISO
+        weekday number (%u).
+        """
+        date = datetime(2016, 11, 12)
+        seq = 31
+        with self.assertRaises(ValueError):
+            star_barcode.barcode_filename(date, seq)
+
+    def test_year_boundary(self):
+        """barcode_filename should use ISO week year not standard year
+
+        January 1 2017 is the final ISO day (Sunday) in the final ISO
+        week of 2016 (52), so the fileame should be:
+            Barcode_2016-W52-7-SS.pdf
+        """
+        date = datetime(2017, 1, 1)
+        seq = 27
+        name = star_barcode.barcode_filename(date, seq)
+        self.assertEqual(
+            name,
+            'Barcode_2016-W52-7-27.pdf'
+            )
+
+
 if __name__ == '__main__':
     unittest.main(verbosity=2)
